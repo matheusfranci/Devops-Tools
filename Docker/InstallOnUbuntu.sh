848a0669d43ccd86c835f29c208d2e44c82374e7
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2024-05-27 20:47:04 -0300

    Create InstallOnUbuntu.sh

---
+++ Docker/InstallOnUbuntu.sh
@@ -0,0 +1,23 @@
+# Adicionando a GPG key oficial:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Adicionando ao repositório do servidor:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+# Atualização do servidor
+sudo apt-get update
+
+# Instalação do docker na última versão
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Verificação pós intalação
+sudo docker run hello-world
+
+systemcrl status docker
